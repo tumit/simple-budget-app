a3f1a84600b0fff9b58f0763630072c30ff1a755
tumit <tumit@live.com>
2023-04-22 16:13:21 +0700

    feat: add adminGuard for requirement-approval

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RequirementListComponent } from './requirement-list/requirement-list.component';
 import { RequirementFormComponent } from './requirement-form/requirement-form.component';
 import { RequirementApprovalComponent } from './requirement-approval/requirement-approval.component';
+import { adminGuard } from './auth/admin.guard';
 
 // map URL => Component
 
@@ -10,7 +11,7 @@ const routes: Routes = [
   { path: 'requirement-list', component: RequirementListComponent },
   { path: 'requirement-form', component: RequirementFormComponent },
   { path: 'requirement-form/:id', component: RequirementFormComponent },
-  { path: 'requirement-approval', component: RequirementApprovalComponent },
+  { path: 'requirement-approval', component: RequirementApprovalComponent, canActivate: [adminGuard] },
   {
     path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   }
--- src/app/auth/admin.guard.ts
+++ src/app/auth/admin.guard.ts
@@ -5,6 +5,11 @@ import {
 } from '@angular/router';
 import { AuthService } from './auth.service';
 
+// requirement-approval => next
+// next ?
+// user.role === 'A' => true => next ได้ไปต่อ => requirement-approval
+// user.role != 'A => routing ใหม่จาก next แต่ตัวสุดท้ายที่ต้องไปคือ /auth/login
+
 export const adminGuard = (next: ActivatedRouteSnapshot) => {
   const loggedInUser = inject(AuthService).getLoggedInUser();
   return loggedInUser?.user.role === 'A'
