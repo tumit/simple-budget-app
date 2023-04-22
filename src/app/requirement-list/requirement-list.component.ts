bb14b4ac11281561282d20df41206a0f80e40a66
tumit <tumit@live.com>
2023-04-22 10:19:26 +0700

    fear: add service

--- src/app/requirement-list/requirement-list.component.ts
+++ src/app/requirement-list/requirement-list.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Requirement } from '../requirement';
+import { RequirementService } from '../requirement.service';
 
 @Component({
   selector: 'app-requirement-list',
   templateUrl: './requirement-list.component.html',
   styleUrls: ['./requirement-list.component.css']
 })
-export class RequirementListComponent {
-  requirements: Requirement[] = [
-    { id: 2000, title: 'USB wire', contactMobileNo: '0891234567' },
-    { id: 2001, title: 'USB A', contactMobileNo: '0991234567' },
-    { id: 2002, title: 'USB C', contactMobileNo: '0881234567' }
-  ]
+export class RequirementListComponent implements OnInit {
+
+  requirements: Requirement[] = []
+
+  constructor(private requirementService: RequirementService) {
+  }
+
+  ngOnInit(): void {
+    // this.requirements
+    //   = requirementService.getRequirements();
+    this.requirementService.getRequirements().subscribe(rs => this.requirements = rs)
+  }
+
 }
--- src/app/requirement.service.ts
+++ src/app/requirement.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Requirement } from './requirement';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RequirementService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getRequirements(): Observable<Requirement[]> {
+    const url = 'http://localhost:3000/requirements';
+    return this.httpClient.get<Requirement[]>(url);
+    // return [
+    //   { id: 2000, title: 'USB wire', contactMobileNo: '0891234567' },
+    //   { id: 2001, title: 'USB A', contactMobileNo: '0991234567' },
+    //   { id: 2002, title: 'USB C', contactMobileNo: '0881234567' },
+    // ];
+  }
 }
