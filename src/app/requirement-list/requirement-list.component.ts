38936ca39fe078e49948e1d0c76de21d1e89ede9
tumit <tumit@live.com>
2023-04-22 09:53:20 +0700

    feat(requirements): add if for for mockData

--- src/app/requirement-list/requirement-list.component.ts
+++ src/app/requirement-list/requirement-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Requirement } from '../requirement';
 
 @Component({
   selector: 'app-requirement-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./requirement-list.component.css']
 })
 export class RequirementListComponent {
-
+  requirements: Requirement[] = [
+    { id: 2000, title: 'USB wire', contactMobileNo: '0891234567' },
+    { id: 2001, title: 'USB A', contactMobileNo: '0991234567' },
+    { id: 2002, title: 'USB C', contactMobileNo: '0881234567' }
+  ]
 }
